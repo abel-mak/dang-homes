3b2113d2d71b0b3d245e89ee6c9efb2a1c4d737e
abdelali <abdelmkey19973@gmail.com>
2024-04-08 00:17:35 +0000

    init

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
+import { gql } from "@apollo/client";
+import client from 'client'
+
+
 export default function Home() {
   return <div>Next JS &amp; WordPress course.</div>;
+}
+
+export const getStaticProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+    query NewQuery {
+      pages {
+        nodes {
+          title
+        }
+      }
+    }
+    `
+  })
+  console.log(data.pages.nodes);
+  return { props: {} }
 }
